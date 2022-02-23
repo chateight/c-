e9e254465fc48d00c8bc70ff94a9630c403fa97f
ryuichi <rusami.usual@icloud.com>
2022-02-23 19:45:35 +0900

    #copy-constructor was added

--- class.cpp
+++ class.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 // class with "const" function
 class A{
     int v;
@@ -17,70 +19,85 @@ int A::get() const{
     return v;
 }
 
-// constructor & destructor
+// constructor, copy-constructor & destructor
 class c_class{
-    int age;
-    std::string name;
+    int age = -1;               // set to meaningless value
+    string name = "hoge";       //
 
-    c_class(int age, std::string name);                 // delegated constructor could be private scope
-
+    c_class(int age, string name);                  // delegated constructor could be private scope
+ 
 public:
     double *m_alloc;
 
-    c_class();
+    explicit c_class();
+    explicit c_class(const c_class& cp);                     // copy constructor
     ~c_class();
 
-    void set_name(std::string s_name);
+    void set_name(string s_name);
     void set_age(int s_age);
 
-    std::string g_name();
-    int g_age();
+    string g_name() const;
+    int g_age() const;
 };
 
-c_class::c_class(int s_age, std::string s_name)         // delegated constructor                            
-    :age(s_age), name(s_name)                           // initialize variables
+c_class::c_class(int s_age, string s_name)          // delegated constructor                            
+    :age(s_age), name(s_name)                       // initialize variables
 {
-    std::cout << "delegeted constructor is called " << std::endl;
+    cout << "delegeted constructor is called " << endl;
 }
 
 c_class::c_class()                                      // constructor : if you don`t define it, compiler generate a default constructor
-    :c_class(30, "bobby")                               // delegate to the "c_class(int age, std::string name)"
+    :c_class(30, "bobby")                               // delegate to the "c_class(int age, string name)"
 {
     m_alloc = new double[1000];                         // memory alloc
-    std::cout << "constructor \"common\" is called " << std::endl;
+    cout << "constructor \"common\" is called " << endl;
 }
 
 c_class::~c_class(){                                    // destructor
     delete [] m_alloc;                                  // memory release
-    std::cout << "destructor is called" << std::endl;   // destructor call timing is decided by c++ compiler
+    cout << "destructor is called" << endl;     // destructor call timing is decided by c++ compiler
+}                                               // called twice, since we make two instances
+
+c_class::c_class(const c_class& cp){
+    cout << "copy constructor is called" << endl;        // "constant" is used to protect original instance
+    m_alloc = new double[1];                    // need to copy resource allocation of the original instance
+    set_name(cp.g_name());
+    set_age(cp.g_age());                        // "age = cp.g_age();"" acts as same as above
 }
 
 void c_class::set_age(int s_age){
     age = s_age;
 }
 
-std::string c_class::g_name(){
+void c_class::set_name(string s_name){
+    name = s_name;
+}
+
+string c_class::g_name() const{
     return name;
 }
 
-int c_class::g_age(){
+int c_class::g_age() const{
     return age;
 }
 
+
+// main process from here
 int main(){
     A a;
     a.set(32);
 
     const A& ca = a;        // ca is a reference to the instance a
     a.set(70);
-    std::cout << "--- constant function of the class ---" << std::endl;
-    std::cout << "const function : " << ca.get() << std::endl;      // -> 70
+    cout << "--- constant function of the class ---" << endl;
+    cout << "const function : " << ca.get() << endl;    // -> 70
     //ca.set(10);           // error, since it tries to change the const instance
 
     // constructor & destructor
-    std::cout << "--- constructor & destructor ---" << std::endl;
+    cout << "--- constructor & destructor ---" << endl;
     c_class cc;
-    std::cout << "age : "<< cc.g_age() << std::endl;
+    c_class cp(cc);                                     // "c_class cp = cc;" does not work since the constructor was defined as explicit
+    cout << "age : "<< cp.g_age() << endl;              // access to the copied instance
     cc.set_age(28);
-    std::cout << "name : "<< cc.g_name() << std::endl;
+    cout << "name : "<< cc.g_name() << endl;
 }
