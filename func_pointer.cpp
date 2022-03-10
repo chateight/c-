12ab83d2e93abee546a3178fdd436224ae038a66
ryuichi <rusami.usual@icloud.com>
2022-03-10 19:41:19 +0900

    #typedef comment

--- func_pointer.cpp
+++ func_pointer.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void (*FUNC_POINTER)(int, int);
+typedef void (*FUNC_POINTER)(int, int);     // obsolete stype, using is for c++
 
 // callback function
 void add(int a, int b){
