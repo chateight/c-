84ba209f71ed72d29d40eda86731651165784885
ryuichi <rusami.usual@icloud.com>
2022-03-25 19:51:31 +0900

    # format & getline()

--- in_out_system.cpp
+++ in_out_system.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cmath>
 
 //
 // main rooutine from here
 int main(){
     auto deft = std::cout.flags();                  // save default flags
     
+    std::cout << std::endl;
     std::cout << "----- output format -----" << std::endl;
     std::cout.setf(std::ios::scientific);
     std::cout << "scientific : " << 971.52f << std::endl;
@@ -18,5 +20,31 @@ int main(){
     std::cout.flags(deft);                                          // override flags()
     std::cout << "reset to dafault : " << 4282.98f << std::endl;
     std::cout << 772340 << std::endl;
+
+    std::cout <<std::endl;
+    std::cout.precision(10);                                        
+    std::cout << "pi = " << M_PI << std::endl;                      // PI
+    std::cout.setf(std::ios::scientific);
+    std::cout << "pi(scientific) = " << M_PI << std::endl;          // it seems the definition of "precision" is different at "scientific" mode
+
+
+    std::cout.width(12);
+    std::cout.setf(std::ios::oct, std::ios::basefield);             // set octal print
+    std::cout.fill('-');                                            // filler "-"
+    std::cout << "octal & filler : "<< 3577957 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "----- consol input -----" << std::endl;
+
+    char buff[256];
+    while(true){
+        std::cin.getline(buff, 256);                                // consol input
+        if (*buff != '\0'){                                         // empty(return only)?  ; "*buff" or "buff[0]" has the same meaning
+            std::cout << "input : " << buff << std::endl;
+        }
+        else{
+            break;                              // if empty -> break
+        }
+    }
 
 }
