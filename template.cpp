3b58f5f13d30edcb04c91850823879bc26eddbf2
ryuichi <rusami.usual@icloud.com>
2022-03-29 17:46:48 +0900

    # class template

--- template.cpp
+++ template.cpp
@@ -13,14 +13,39 @@ public:
 };
 
 //
+// class temlate
+template <typename CT>
+class ct{
+
+public:
+CT val;
+
+void ct_f();
+};
+
+template <typename CT>
+void ct<CT>::ct_f(){
+    std::cout << val << std::endl;
+}
+
+//
 // main routine from here
 int main()
 {
     S s;
 
+    std::cout << "----- template function -----" << std::endl;
     std::cout << " int : "<< s.foo<int>(5) << std::endl;
     std::cout << " float : " << s.foo<float>(53.238f) << std::endl;
     std::cout << " char : " << s.foo<char>('d') << std::endl;
     std::cout << " string : " << s.foo<std::string>("hello") << std::endl;
 
+    std::cout << std::endl;
+    std::cout << "----- template class -----" <<std::endl;
+    ct<float> tc{52.3f};
+    tc.ct_f();
+
+    ct<std::string> tc_s{"bigger"};
+    tc_s.ct_f();
+    
 }
