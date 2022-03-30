f3c36e2a9f2a04293df4ffd7a901988c7aaaf178
ryuichi <rusami.usual@icloud.com>
2022-03-30 17:32:19 +0900

    # using vitual function in a template class

--- template.cpp
+++ template.cpp
@@ -29,6 +29,27 @@ void ct<CT>::ct_f(){
 }
 
 //
+// use virtual function in a class template
+class ctv{
+
+public:
+    virtual void show(float fl){
+        std::cout << "base : " << fl << std::endl;
+    }
+
+};
+
+template <typename T>
+class d_ctv : public ctv{
+
+public:
+    void show(T vbl) override{
+        std::cout << "override : " << vbl << std::endl;
+    }
+
+};
+
+//
 // main routine from here
 int main()
 {
@@ -47,5 +68,12 @@ int main()
 
     ct<std::string> tc_s{"bigger"};
     tc_s.ct_f();
-    
+
+    std::cout << std::endl;
+    std::cout << "----- using virtual function in a template class -----" <<std::endl;
+    d_ctv<float> vin;
+    vin.show(28);
+    vin.ctv::show(21);
+    static_cast<ctv&>(vin).show(27);        // call via virtual function( result is same as above )
+
 }
