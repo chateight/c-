32a8381a0ff7a2e5a9cdbebb7c95cc0894c03063
ryuichi <rusami.usual@icloud.com>
2022-04-05 21:30:24 +0900

    # some updates

--- variadic_temp_func.cpp
+++ variadic_temp_func.cpp
@@ -3,7 +3,7 @@
 template <class... Args>
 void func2()
 {
-    std::cout << "no args func is called\n" << std::endl;
+    std::cout << "no arg func is called\n" << std::endl;
 }
 
 template <class Head, class... Body>
@@ -11,11 +11,12 @@ void func2(Head head, Body... body)
 {
     std::cout << head << "\t" << sizeof...(body) << "\n";
 
-    func2(body...);
+    func2(body...);         // recursive call 
 }
 
 int main()
 {
     func2("foo", "bar", "hoge", "huga");
+    func2("one", "two", "three");
     func2();
 }
