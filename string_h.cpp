14647f46b0943689c3abd9629a26ae1d46bc97d6
ryuichi <rusami.usual@icloud.com>
2022-04-19 19:11:56 +0900

    # regular expression

---
+++ string_h.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <charconv>
+#include <regex>
+
+//
+// main method from here
+int main(){
+
+    std::cout << std::endl;
+    std::cout << "----- string basic -----" << std::endl;
+    std::string str("where is a heaven?");
+
+    std::string str_p(str, 10, std::string::npos);  // pick up the sub-string using new constructor
+    std::cout << "substring is : "<< str_p << std::endl;
+
+    char* chr = str.data();
+    std::cout << "first charactor is : " << *chr << std::endl;
+
+    std::string a{"abcdefghijk"};
+    std::string b{"abcdefghijl"};
+
+    int ret = a.compare(b);
+    std::cout << "compared result : " << ret << std::endl;  // b < a, since dictionsry order is applied
+
+    str += " it's inside yours ";       //combine the strings
+    std::cout << str << std::endl; 
+
+    std::cout << std::endl;
+    std::cout << "----- conversion between string and numeric -----" << std::endl;
+
+    std::string st_i = std::to_string(218);
+    std::cout << "integer to string : " << st_i << std::endl;
+
+    float l_st = std::stof("64.189", nullptr);      // second argument "nullptr" does not need
+    std::cout << "string to float : " << l_st << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "----- basic regular expression -----" << std::endl;
+
+    std::regex re{"\\d+"};                  // regex pattern definition
+    std::string in = "fhgtg24tfhkgjo34442iphhyip";
+    std::smatch match;                      // matched string
+    
+    while (std::regex_search(in, match, re)){
+        std::cout << match.str() << std::endl;
+        in = match.suffix();                // rest of the string after the match
+        std::cout << in << std::endl;
+    }
+
+}
