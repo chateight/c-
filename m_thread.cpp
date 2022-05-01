e89af2f0f19895227faf14bfeb15b7e1185dfe29
ryuichi <rusami.usual@icloud.com>
2022-05-01 16:09:16 +0900

    # correct the comment

--- m_thread.cpp
+++ m_thread.cpp
@@ -7,7 +7,7 @@
 std::mutex mtx_;                // mutex for exclusive control
 std::vector<int> vec{};         // prime numbers array
 int cal_int = 1;                // calc target integer
-int max_int = 10000000;          // calc target max number
+int max_int = 10000000;         // calc target max number
 
 
 void add_prime(int i)
