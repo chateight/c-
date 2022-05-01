20d2e27022a73c7fa9111380a89b471767669abf
ryuichi <rusami.usual@icloud.com>
2022-05-01 18:51:19 +0900

    # correct typo

--- rtti_cast.cpp
+++ rtti_cast.cpp
@@ -2,12 +2,12 @@
 #include <typeinfo>
 
 //
-// same delta expression has defferent type
-auto delta_a = [](std::string str){
+// same lambda expression has defferent type
+auto lambda_a = [](std::string str){        // if there is no argument, you can delete "()"
     std::cout << str << std::endl;
 };
 
-auto delta_b = [](std::string str){
+auto lambda_b = [](std::string str){
     std::cout << str << std::endl;
 };
 
@@ -61,13 +61,13 @@ const char* show_name()
 int main(){
 
     std::cout << std::endl;
-    std::cout << "----- does the same delta expressions have same type? -----" << std::endl;
+    std::cout << "----- does the same lambda expressions have same type? -----" << std::endl;
     
-    if ( typeid(delta_a) == typeid(delta_b)){
-        std::cout << "same delta expression has same type" << std::endl;
+    if ( typeid(lambda_a) == typeid(lambda_b)){
+        std::cout << "same lambda expression has same type" << std::endl;
     }
     else{
-        std::cout << "same delta expression has defferent type" << std::endl;
+        std::cout << "same lambda expression has defferent type" << std::endl;
 
     }
 
